5d9243008c4fcd9511e526db7b8d10ad88063321
Lukasz  Bielak <2bielak@stud235.ftj.agh.edu.pl>
2016-11-09 14:23:18 +0100

    aa

--- kol2_gr2.py
+++ kol2_gr2.py
@@ -14,3 +14,39 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+#!/usr/bin/env python2.6
+
+from collections import defaultdict
+
+class Student(object):
+	def __init__(self,name,sname):
+		self.fname = name
+		self.sname = sname
+		self.score = {}
+		
+	
+	def add_grade(self,clas,grade):
+		self.score.setdefault(clas, []).append(grade)
+		
+
+class Class(object):
+	def __init__(self,name):
+		self.name = name
+		self.Students = []
+	
+	def add_stud(self,Student):
+		if Student not in self.Students:
+			self.Student = self.Student.append(Student)
+
+	def add_grad(self,Student,grade):
+		if Student in self.Students:
+			Student.add_grade(self.name,grade)
+
+	def add_att(self,Student,grade):
+		if Student in self.Students:
+			Student.add_grade(self.name,grade)
+
+
+
+
